c55ce49686d06abf5ea6fbcce88d08fbc45b1b81
codeskiii <piotrpawelec0000@gmail.com>
2024-08-20 23:18:52 +0200

    blank

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,56 @@
+<!-- source: https://codepen.io/shubhamjainco/pen/oPEzrV -->
+
+<script lang="ts">
+    import { getCookie } from 'typescript-cookie';
+    import { onMount } from 'svelte';
+
+    let user: string = "Login/Register";
+
+    async function user_collect() {
+        const sessionID = getCookie('sessionID');
+
+        if (!sessionID) {
+            return;
+        }
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/check/', {
+                method: 'GET',
+                headers: {
+                    'Session': sessionID
+                }
+            });
+
+            if (!response.ok) {
+                return;
+            }
+
+            const apiResponse = await response.json();
+            user = apiResponse.username;
+
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            user = "Login/Register";
+        }
+    }
+
+    onMount(async () => {
+        await user_collect();
+    });
+</script>
+
+<nav class="uk-container uk-navbar">
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active">
+                <a href="/" class="uk-text-large" style="font-size: 160%;">RUST<strong>CASINO</strong></a>
+            </li> 
+        </ul>
+    </div>
+    <div class="uk-navbar-right">
+        <ul class="uk-navbar-nav uk-visible@s">
+            <li><a class="uk-text-large" style="font-size: 110%;" href="/">blank</a></li>
+            <li><a class="uk-text-large" style="font-size: 110%;" href="/">{ user }</a></li>
+        </ul>
+    </div>
+</nav>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,44 +1,46 @@
 <!-- src/routes/example/+page.svelte -->
+
 <script lang="ts">
   export let data;
 
   let games = data["props"]["data"]["games"]; 
   let lotteries = data["props"]["data"]["lotteries"];
 
-  console.log(games);
 </script>
- 
-{#if games.length > 0}
-  <!-- <ul> 
-    {#each games as game, index}
-      <li>
-        <h2>{index + 1}. {game.name}</h2>
-        <p>{game.description}</p>
-        <ul>
-          {#each game.cryptos as crypto}
-            <li>{crypto}</li>
-          {/each}
-        </ul>
-      </li>
-    {/each}
-  </ul> -->
 
-  <div class="uk-grid">
-    {#each games as game}
+<div>
+    {#if games.length > 0}
+    <!-- <ul> 
+      {#each games as game, index}
+        <li>
+          <h2>{index + 1}. {game.name}</h2>
+          <p>{game.description}</p>
+          <ul>
+            {#each game.cryptos as crypto}
+              <li>{crypto}</li>
+            {/each}
+          </ul>
+        </li>
+      {/each}
+    </ul> -->
 
-    <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-margin-medium-top">
-      <h3 class="uk-card-title">{game.name}</h3>
-      <p>{game.description}</p>
-      <p class="uk-text-bold">Entry cost: {game.entry_cost}</p>
-      <p class="uk-text-bold">Max multiplier: {game.prize_multiplier}x</p>
-    </div>
-
-    {/each}
+    <div class="uk-grid uk-child-width-1-3@m uk-grid-match uk-flex-center uk-card-small">
+      {#each games as game}
+          <div>
+              <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-margin-medium-top">
+                  <h3 class="uk-card-title">{game.name}</h3>
+                  <p>{game.description}</p>
+                  <p class="uk-text-bold">Entry cost: {game.entry_cost}</p>
+                  <p class="uk-text-bold">Max multiplier: {game.prize_multiplier}x</p>
+              </div>
+          </div>
+      {/each}
   </div>
 
-{:else}
+  {:else}
   <p>Failed Loading</p>
-{/if}
+  {/if}
+</div>
 
 <!-- 
 <pre>{JSON.stringify(data, null, 2)}</pre>
