cad93aa35a883409fd1d2252b350615af1b5991a
codeskiii <piotrpawelec0000@gmail.com>
2024-08-28 13:52:05 +0200

    not ready login page lol

--- frontend/src/routes/logging_page/+page.svelte
+++ frontend/src/routes/logging_page/+page.svelte
@@ -1,12 +1,40 @@
-<script>
+<script lang="ts">
+	// Function to compute SHA-1 hash using Web Crypto API
+
+	async function sha1Hash(message: string): Promise<string> {
+	  const encoder = new TextEncoder();
+	  const data = encoder.encode(message);
+  
+	  const hashBuffer = await crypto.subtle.digest('SHA-1', data);
+	  const hashArray = Array.from(new Uint8Array(hashBuffer));
+	  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  
+	  return hashHex;
+	}
+  
 	let username = '';
 	let password = '';
-
-	function logging_service() {
-		console.log("Username:", username);
-		console.log("Password:", password);
+  
+	async function logging_service() {
+	  console.log("Username:", username);
+	  console.log("Password:", password);
+  
+	  const hashedPassword = await sha1Hash(password);
+  
+	  const login_response = await fetch("http://127.0.0.1:8000/logging/", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'Authorization',
+		},
+		body: JSON.stringify({
+			username: 'user',
+			password: 'hashedPassword',
+		}),
+	});
+	
+	  console.log(login_response);
 	}
-</script>
+  </script>
 
 <div class="uk-section uk-section-muted uk-flex uk-flex-middle uk-animation-fade" uk-height-viewport>
 	<div class="uk-width-1-1">
